51d7590df00df927aa30db657debd16e904be20b
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-07-09 17:20:47 +0800

    feat: update home page

---
+++ scripts/home.py
@@ -0,0 +1,25 @@
+import os
+import json
+
+
+def generate():
+    directory = "../slides"
+
+    contents = []
+    for item in os.listdir(directory):
+        readme = os.path.join(directory, item, "README.md")
+        if not os.path.isfile(readme):
+            continue
+        with open(readme, 'r', encoding='utf-8') as f:
+            title = f.read().strip()
+        contents.append({
+            "path": item,
+            "title": title
+        })
+
+    with open("src/slides.json", 'w', encoding='utf-8') as f:
+        json.dump(contents, f, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    generate()
